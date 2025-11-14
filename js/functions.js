e95e77e3a187e1a93e261f2960062cdf33fa0eb3
sarthak-chakraborty <sarthak.chakraborty@gmail.com>
2025-11-13 19:19:07 -0600

    javascript modified

--- js/functions.js
+++ js/functions.js
@@ -3,11 +3,24 @@ function showEmail() {
     "sc134 [at] illinois [dot] edu"    
 }
 
-function toggleVis(id) {
-  var e = document.getElementById(id)
+// function toggleVis(id) {
+//   var e = document.getElementById(id)
+//   if (e.style.display == "none") {
+//     e.style.display = "block";
+//   } else {
+//     e.style.display = "none";
+//   }
+// }
+
+
+function toggleVis(news_id, link_id) {
+  var e = document.getElementById(news_id)
+  var link = document.getElementById(link_id)
   if (e.style.display == "none") {
     e.style.display = "block";
+    link.innerHTML = "show less";
   } else {
     e.style.display = "none";
+    link.innerHTML = "show more";
   }
 }
